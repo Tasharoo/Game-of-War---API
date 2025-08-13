7e1c79d001fc5e2ef926b168cb51f200a842c85d
Tasharoo <Tasha357uk@gmail.com>
2025-08-13 19:05:07 +0100

    *yes

--- index.js
+++ index.js
@@ -10,54 +10,56 @@ const newDeckButton = document.getElementById("new-deck");
 const newCardButton = document.getElementById("new-card");
 const winner = document.getElementById("final-winner");
 const resetBtn = document.getElementById("reset-btn");
-
-async function handleClick() {
-  const res = await fetch(
+function handleClick() {
+  fetch(
     `https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/?deck_count=${deckCount}`
-  );
-  const data = await res.json();
-  deckId = data.deck_id;
-  if (data.success) {
-    cardInfo.innerHTML = `Deck successfully shuffled!
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      deckId = data.deck_id;
+      if (data.success) {
+        cardInfo.innerHTML = `Deck successfully shuffled!
          <h2>Cards remaning : ${data.remaining}</h2>
        `;
-  }
+      }
+    });
 }
-async function handleDeck() {
-  const res = await fetch(
-    `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
-  );
-  const data = await res.json();
-  if (!deckId) {
-    cardInfo.innerHTML = "Please shuffle a new deck first!";
-    computerCards.classList.add("card-placeholder");
-    youCards.classList.add("card-placeholder");
-  } else if (data.remaining === 0) {
-    cardInfo.innerHTML = "No more cards left in the deck! shuffle a new deck!";
-    newCardButton.disabled = true;
-  }
+function handleDeck() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!deckId) {
+        cardInfo.innerHTML = "Please shuffle a new deck first!";
+        computerCards.classList.add("card-placeholder");
+        youCards.classList.add("card-placeholder");
+      } else if (data.remaining === 0) {
+        cardInfo.innerHTML =
+          "No more cards left in the deck! shuffle a new deck!";
+        newCardButton.disabled = true;
+      }
 
-  const winderText = whosTheWinner(data.cards[0], data.cards[1]);
-  cardInfo.innerHTML = ` <h2>Cards remaning : ${data.remaining}</h2>`;
-  computerCards.innerHTML = ` 
+      const winderText = whosTheWinner(data.cards[0], data.cards[1]);
+      cardInfo.innerHTML = ` <h2>Cards remaning : ${data.remaining}</h2>`;
+      computerCards.innerHTML = ` 
           <h2>${winderText}</h2>
           <h3>Computer: ${computerScore}</h3>
      <img class="card-placeholder" src=${data.cards[0].image}></button>`;
-  youCards.innerHTML = `
+      youCards.innerHTML = `
         <img class="card-placeholder" src=${data.cards[1].image}></button>
      <h3>You: ${playerScore}</h3>`;
-  computerCards.classList.remove("card-placeholder");
-  youCards.classList.remove("card-placeholder");
+      computerCards.classList.remove("card-placeholder");
+      youCards.classList.remove("card-placeholder");
 
-  if (data.remaining === 0) {
-    if (computerScore > playerScore) {
-      winner.innerHTML = "Computer Wins the game!";
-    } else if (playerScore > computerScore) {
-      winner.innerHTML = "Player Wins the game!";
-    } else {
-      winner.innerHTML = "It's a tie game!";
-    }
-  }
+      if (data.remaining === 0) {
+        if (computerScore > playerScore) {
+          winner.innerHTML = "Computer Wins the game!";
+        } else if (playerScore > computerScore) {
+          winner.innerHTML = "Player Wins the game!";
+        } else {
+          winner.innerHTML = "It's a tie!";
+        }
+      }
+    });
 }
 function resetScore() {
   playerScore = 0;
